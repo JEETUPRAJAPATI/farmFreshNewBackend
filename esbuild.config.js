ea806ba8e24b7fea67a4ee32be954c321eb08834
jeetu-mj <jeetu.mj123@gmail.com>
2025-06-07 14:49:55 +0530

    Update new Code 2025-06-07

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,14 +1,10 @@
-// esbuild.config.js
-import { build } from 'esbuild'
-import path from 'path'
+import { build } from 'esbuild';
 
 build({
     entryPoints: ['src/index.ts'],
     bundle: true,
-    outfile: 'dist/index.js',
-    platform: 'node',
     format: 'esm',
-    alias: {
-        '@shared': path.resolve(__dirname, 'src/shared'),
-    },
-})
+    platform: 'node',
+    outdir: 'dist',
+    packages: 'external',
+}).catch(() => process.exit(1));
